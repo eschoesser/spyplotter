c758dcfa4d11b71124f3d892c010dc08533decd1
Elisa Schösser <elisa.schoesser@uni-heidelberg.de>
2023-10-30 14:59:29 +0100

    add radial velocity

--- spyplotter/spectrum.py
+++ spyplotter/spectrum.py
@@ -4,6 +4,7 @@ import scipy.constants as const
 import numpy as np
 from matplotlib import pyplot as plt
 from astropy import units as u
+from astropy.coordinates import SpectralCoord, SpectralQuantity
 from pathlib import Path
 
 from .powr import readWRPlotDatasets
@@ -12,10 +13,10 @@ logger = setup_log(__name__)
 
 class Spectrum(object):
     
-    def __init__(self, x: ArrayLike, y: ArrayLike, x_unit:u.Unit=None, y_unit:u.Unit=None,name:str=None):
+    def __init__(self, x: ArrayLike, y: ArrayLike, x_unit:u.Unit=None, y_unit:u.Unit=None,name:str=None,vrad=0 * u.km / u.s):
         """Generic class for a spectrum. The same class is used for observed and model spectra
 
-        :param x: array of wavelength or frequency values
+        :param x: array of wavelength or frequency values (if they have unit, keep unit)
         :type x: ArrayLike
         :param y: flux, default: normalized spectrum
         :type y: ArrayLike
@@ -24,23 +25,25 @@ class Spectrum(object):
         :param y_unit: flux unit of the spectrum, default is None = normalized spectrum
         :type y_unit: u.Unit, optional
         :param name: optional name for the spectra, can be used for labelling them
-        :type name: string 
+        :type name: string
+        :param vrad: radial velocity, if only float, use km/s
+        :type vrad: float, SpectralCoord, SpectralQuantity or u.quantity.Quantity
         """
         self.name = name
         
-        if type(x) == u.quantity.Quantity:
+        if isinstance(x,(u.quantity.Quantity,SpectralCoord,SpectralQuantity)):
             # if x has already unit, don't change it
             logger.info(f'Keeping units of x: {x.unit}')
-            self._x = x
+            self._x = SpectralCoord(x)
         elif x_unit == None:
             # if no unit is given, use nm
             logger.info('As no unit for x was specified, Angstrom are assumed')
-            self._x = x * u.AA
+            self._x = SpectralCoord(x * u.AA)
         else:
             # if unit is specified and x does not have unit, take specified unit
-            self._x = x * x_unit
+            self._x = SpectralCoord(x * x_unit)
         
-        if type(y) == u.quantity.Quantity:
+        if isinstance(y,(u.quantity.Quantity,SpectralCoord,SpectralQuantity)):
             # if y has already unit, don't change it
             logger.info(f'Keeping units of y: {y.unit}')
             self._y = y
@@ -60,7 +63,18 @@ class Spectrum(object):
             # if unit is specified and y does not have unit, take specified unit
             self._y = y * y_unit
             self._normalized = False
-        
+            
+        if isinstance(vrad,(u.quantity.Quantity,SpectralCoord,SpectralQuantity)):
+            self._vrad = vrad
+            logger.debug(f'Applying radial velocity shift to spectrum of {vrad}')
+            self._x = self._x.with_radial_velocity_shift(vrad)
+        elif isinstance(vrad,float):
+            self._vrad = vrad
+            logger.info('No unit of vrad specified, thus using km/s.')
+            self._x = self._x.with_radial_velocity_shift(vrad * u.km / u.s)
+        else:
+            logger.error('Not known format for vrad used. Convert to float or astropy classes Quantity, SpectralCoord or SpectralQuantity')
+            raise ValueError
     
     def __call__(self):
         # ToDo: when called at a specific x point, use spline or interpolation to evaluate flux at given point?
@@ -76,7 +90,7 @@ class Spectrum(object):
         return np.array([self._x,self._y])
     
     @property
-    def x(self,unit:u.Unit=None) -> u.quantity.Quantity:
+    def x(self,unit:u.Unit=None):
         """Return x values of spectrum
 
         :param unit: unit of returned value, defaults to None
@@ -90,7 +104,7 @@ class Spectrum(object):
             return self._x.to(unit,equivalencies=u.spectral())
     
     @property
-    def y(self, unit:u.Unit=None) -> u.quantity.Quantity:
+    def y(self, unit:u.Unit=None):
         if unit == None:
             return self._y
         else:
@@ -107,9 +121,13 @@ class Spectrum(object):
     @property
     def normalized(self) -> bool:
         return self._normalized
+    
+    @property
+    def vrad(self):
+        return self._vrad
         
     @classmethod
-    def from_powr(cls, filepath, keywords:List[int]=[''], dataset:int=1, xunit:u.Unit=None,yunit:u.Unit=None,name=None):
+    def from_powr(cls, filepath, keywords:List[int]=[''], dataset:int=1, xunit:u.Unit=None,yunit:u.Unit=None,name=None,vrad=0. * u.km/u.s):
         #ToDo: Check if all key words correspond to normalized or unnormalized spectrum
         
         path = Path(filepath)
@@ -136,10 +154,10 @@ class Spectrum(object):
                 logger.info('No flux unit specified and no signs for y units detected. Thus assuming normalized spectum.')
                 yunit = None
             
-        return cls(x,y,xunit,yunit,name)
+        return cls(x,y,xunit,yunit,name,vrad)
     
     @classmethod
-    def from_file(cls,filename, skiprows:int=0, delimiter:str=' ',xunit:u.Unit=None,yunit:u.Unit=None,name:str=None,**kwargs):
+    def from_file(cls,filename, skiprows:int=0, delimiter:str=' ',xunit:u.Unit=None,yunit:u.Unit=None,name:str=None,vrad=0.*u.km/u.s,**kwargs):
         """read spectrum from a file
 
         :param filename: path and file name
@@ -165,8 +183,7 @@ class Spectrum(object):
             logger.error('Path does not exist')
             raise ValueError
         
-        return cls(data[:,0], data[:,1], xunit, yunit,name)
-        
+        return cls(data[:,0], data[:,1], xunit, yunit,name,vrad)
     
     @classmethod
     def from_cmfgen(cls,filepath,name:str=None):
@@ -191,8 +208,37 @@ class Spectrum(object):
         if y_unit is not None:
             logger.debug(f'The unit of all y values is changed to {y_unit}')
             self._x = self._y.to(y_unit,equivalencies=u.spectral())
-     
-    def plot(self,x_unit:u.Unit=None, y_unit:u.Unit=None, interval:ArrayLike=None, ax=None,**kwargs):
+    
+    def apply_shift_vrad(self,vrad,overwrite=True):
+        """Apply radial shift to spectrum, overwrites current spectrum
+
+        :param vrad: radial velocity
+        :type vrad: float or astropy classes Quantity, SpectralCoord or SpectralQuantity
+        :raises ValueError: if vrad has not one of required formats
+        """
+        if isinstance(vrad,(u.quantity.Quantity,SpectralCoord,SpectralQuantity)):
+            logger.debug(f'Total shift: {self._vrad} (before) + {vrad} (after) = {self._vrad + vrad}')
+            new_vrad = self._vrad + vrad
+        elif isinstance(vrad,(float,int)):
+            logger.info('No unit for vrad specified. Thus assuming km/s.')
+            vrad = vrad * u.km / u.s
+            logger.debug(f'Total shift: {self._vrad} (before) + {vrad} (after) = {self._vrad + vrad}')
+            new_vrad = self._vrad + vrad
+        else:
+            logger.error('Not known format for vrad used. Convert to float or astropy classes Quantity, SpectralCoord or SpectralQuantity')
+            raise ValueError
+        
+        if overwrite:
+            #Overwrite spectrum 
+            logger.debug('Overwrite spectrum when applying velocity shift')
+            self._x = self._x.with_radial_velocity_shift(vrad)
+            self._vrad = new_vrad
+        else:
+            logger.debug('Return new object with shiftet x-values')
+            return Spectrum(self.x,self.y,self.x_unit,self.y_unit,self.name,new_vrad)
+        
+    
+    def plot(self,x_unit:u.Unit=None, y_unit:u.Unit=None, interval:ArrayLike=None, ax=None,fig_width=10,fig_height=4,**kwargs):
         """Function to plot the spectrum
         The units can be changed for the plotting. However, 
         the units are only changed for the plot and not the whole class
@@ -206,10 +252,13 @@ class Spectrum(object):
         :type y_unit: u.Unit, optional
         :param ax: can be specified to plot in a previously created figure, defaults to None
         :type ax: Axis, optional
+        :param fig_width: width of figure, defaults to 10
+        :type fig_width: int, optional
+        :param fig_height: height of figure, defaults to 4
+        :type fig_height: int, optional
         :return: Figure
         """
         
-        #Convert to different 
         #does not convert whole spectrum - only for plotting
         if x_unit is None: 
             logger.debug(f'Using the following pre-specified units:\n\tx:{self._x.unit}')
@@ -227,7 +276,7 @@ class Spectrum(object):
             
         if ax is None:
             
-            fig, ax = plt.subplots()
+            fig, ax = plt.subplots(figsize=(fig_width,fig_height))
 
         else:
 
@@ -261,15 +310,41 @@ class Spectrum(object):
         return fig
     
     def zoom_plot(self,intervals:ArrayLike,fig_width=10,fig_height=4, x_unit:u.Unit=None, y_unit:u.Unit=None,**kwargs):
+        """Plot zooming into specified intervaks
+
+        :param intervals: intervals that are zoomed into
+        :type intervals: list of intervals (interval: list of length 2)
+        :param fig_width: width of figure, defaults to 10
+        :type fig_width: int, optional
+        :param fig_height: height of figure, defaults to 4
+        :type fig_height: int, optional
+        :param x_unit: x unit for plotting - not changed for whole spectrum, defaults to None
+        :type x_unit: u.Unit, optional
+        :param y_unit: y unit for plotting - not changed for whole spectrum, defaults to None
+        :type y_unit: u.Unit, optional
+        :return: figure
+        """
         #ToDo: Add option for specifying multiple columns
         
-        n_int = len(intervals)
-        
-        fig, ax = plt.subplots(n_int,1,figsize=(fig_width,fig_height*n_int))
-        
-        for i in range(n_int):
-            self.plot(x_unit, y_unit, interval=intervals[i],ax=ax[i],**kwargs)
+        #Check if intervals have the right shape (x,2) or (2,)
+        if (len(np.shape(intervals))==2) and (np.shape(intervals)[-1]==2):
+            #multiple intervals
+            n_int = len(intervals)
+            fig, ax = plt.subplots(n_int,1,figsize=(fig_width,fig_height*n_int))
+            for i in range(n_int):
+                self.plot(x_unit, y_unit, interval=intervals[i],ax=ax[i],**kwargs)
+                
+        elif (len(np.shape(intervals))==1) and (np.shape(intervals)[-1]==2):
+            #only one interval
+            n_int = 1
+            fig, ax = plt.subplots(n_int,1,figsize=(fig_width,fig_height*n_int))
+            #Only one interval
+            self.plot(x_unit, y_unit, interval=intervals,ax=ax,**kwargs)
             
+        else:
+            logger.error(f'Intervals have the wrong shape {np.shape(intervals)}. Instead, they need to have the shape (x,2) or (2,)')
+            raise ValueError
+        
         return fig
     
     
