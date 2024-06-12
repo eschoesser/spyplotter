27d06f08a1ffffed3812b4d4d87dab1c36e82a7d
Elisa Schösser <elisa.schoesser@uni-heidelberg.de>
2024-06-12 17:17:46 +0200

    fix pip install

--- spyplotter/utils/package_data.py
+++ spyplotter/utils/package_data.py
@@ -3,5 +3,10 @@ import pkg_resources
 
 
 def get_path_of_config_file(config_file) -> Path:
-    file_path = pkg_resources.resource_filename("spyplotter", "config/%s" % config_file)
+    file_path = pkg_resources.resource_filename("spyplotter", f"config/{config_file}")
+    return file_path
+
+
+def get_path_of_data_file(data_file) -> Path:
+    file_path = pkg_resources.resource_filename("spyplotter", "data/%s" % data_file)
     return file_path
